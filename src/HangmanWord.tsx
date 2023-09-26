f7a619f84b1a24d5dda4ac6ca33968743225bdcd
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2023-09-25 23:44:01 -0500

    word

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,13 @@
 export function HangmanWord() {
-    
+    return <div
+        style={{
+            display: "flex",
+            gap: ".25em",
+            fontSize: "6rem",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            fontFamily: "monospace"
+        }}>
+        Test
+    </div>
 }
