c7d6aefb5494a56a9f3e3b0aaa72c9c33af6a9a8
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2023-09-25 23:25:06 -0500

    drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
 
-  console.log(wordToGuess)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  
   return (
-    <h1>
-     Hi
-    </h1>
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems:"center"
+    }}>
+      <div style={{fontSize:"2rem", textAlign: "center"}}>
+        Lose
+        Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
   )
 }
 
